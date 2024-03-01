3ab1820f919d26cc05af2521ca34c5eaba31254c
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2024-03-02 00:10:27 +0600

    Initiate Project

---
+++ PhotoWizard/PhotoWizard/Src/PhotoWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoWizardApp.swift
+//  PhotoWizard
+//
+//  Created by Shahwat Hasnaine on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
